a286dfa60b09092bf38e01b659463b5ac2667d17
keisuke taniguchi <rememberthatk@gmail.com>
2018-04-06 01:52:01 +0000

    mongooseに接続できるようにする

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'oauthServerSampleSecret',
-  'database': 'mongodb://$MONGO_PORT_27017_TCP_ADDR/jwt_server',
+  'database': `mongodb://${process.env.MONGO_PORT_27017_TCP_ADDR}/jwt_server`,
 };
